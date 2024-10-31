a61060586294f8453f71dd0ff936dac38392d73a
unrealperson <34791074+unrealperson@users.noreply.github.com>
2024-10-31 20:57:45 +0100

    Exercise 3: downsize

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import linalg
+import subprocess
 
 class Image:
 
@@ -13,11 +14,19 @@ class Image:
 
     rgb_from_yuv_mat = linalg.inv(yuv_from_rgb_mat)
 
+    @staticmethod
     def yuv_from_rgb(rgb):     
         return rgb @ Image.yuv_from_rgb_mat.T
-    
+
+    @staticmethod
     def rgb_from_yuv(yuv):
         return yuv @ Image.rgb_from_yuv_mat.T
+
+    @staticmethod
+    def downsize(image_file_name, ratio = 2):
+        command = 'ffmpeg -i {} -vf scale=iw/{}:ih/{} small{} -y'
+        command = command.format(image_file_name, ratio, ratio, image_file_name)  # Replace 'image.jpg' with your actual file name.
+        subprocess.run(command.split(sep=' '), shell=True)
 
 
 rgb = np.array([100, 54, 206])
@@ -25,3 +34,5 @@ yuv = Image.yuv_from_rgb(rgb)
 rgb2 = Image.rgb_from_yuv(yuv)
 print(yuv)
 print(rgb2)
+
+Image.downsize("1.png", 3)
