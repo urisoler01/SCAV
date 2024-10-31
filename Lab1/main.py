979a3777965c53d5c0a0ab57588e93a946c2006f
urisoler01 <oriolsogo16@gmail.com>
2024-10-31 19:58:55 +0100

    Update main.py
    Part 2 DONE

--- Lab1/main.py
+++ Lab1/main.py
@@ -2,8 +2,8 @@ import numpy as np
 from scipy import linalg
 
 class Image:
-       
-    yuv_from_rgb = np.array(
+
+    yuv_from_rgb_mat = np.array(
         [
             [0.299, 0.587, 0.114],
             [-0.14714119, -0.28886916, 0.43601035],
@@ -11,11 +11,17 @@ class Image:
         ]
     )
 
-    rgb_from_yuv = linalg.inv(yuv_from_rgb)
+    rgb_from_yuv_mat = linalg.inv(yuv_from_rgb_mat)
 
     def yuv_from_rgb(rgb):     
-        return rgb @ yuv_from_rgb.T.astype(arr.dtype)
+        return rgb @ Image.yuv_from_rgb_mat.T
+    
     def rgb_from_yuv(yuv):
-        return yuv @ rgb_from_yuv.T.astype(arr.dtype)
+        return yuv @ Image.rgb_from_yuv_mat.T
 
-Image.yuv_from_rgb('rgb')
+
+rgb = np.array([100, 54, 206])
+yuv = Image.yuv_from_rgb(rgb)
+rgb2 = Image.rgb_from_yuv(yuv)
+print(yuv)
+print(rgb2)
