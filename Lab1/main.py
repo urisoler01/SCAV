cc3a4723f3c5fd51d16db99aade3e7891ec40ef2
urisoler01 <oriolsogo16@gmail.com>
2024-10-31 19:42:17 +0100

    Just to show

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,11 +1,20 @@
-
+import numpy as np
+from scipy import linalg
 
 class Image:
-    def yuv_from_rgb(rgb):
-        conversion_mat = np.array(
+       
+    yuv_from_rgb = np.array(
         [
             [0.299, 0.587, 0.114],
             [-0.14714119, -0.28886916, 0.43601035],
             [0.61497538, -0.51496512, -0.10001026],
         ]
     )
+
+    rgb_from_yuv = linalg.inv(yuv_from_rgb)
+
+    def yuv_from_rgb(rgb):     
+        return rgb @ yuv_from_rgb.T.astype(arr.dtype)
+    def rgb_from_yuv(yuv):
+        return yuv @ rgb_from_yuv.T.astype(arr.dtype)
+
